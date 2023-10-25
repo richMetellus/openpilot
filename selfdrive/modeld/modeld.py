4427ba132eaced43e64b06d87557535654866eba
Comma Device <device@comma.ai>
2023-10-24 23:24:10 -0500

    still got 350 out of 48k

--- selfdrive/modeld/modeld.py
+++ selfdrive/modeld/modeld.py
@@ -25,7 +25,7 @@ logging.basicConfig(filename='/tmp/myapp.log', level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(name)s %(message)s')
 logger=logging.getLogger(__name__)
 
-SEND_RAW_PRED = os.getenv('SEND_RAW_PRED')
+SEND_RAW_PRED = True #os.getenv('SEND_RAW_PRED')
 
 MODEL_PATHS = {
   ModelRunner.THNEED: Path(__file__).parent / 'models/supercombo.thneed',
@@ -108,90 +108,13 @@ def main():
   model = ModelState(cl_context)
   cloudlog.warning("models loaded, modeld starting")
 
-  # visionipc clients
-  while True:
-    available_streams = VisionIpcClient.available_streams("camerad", block=False)
-    if available_streams:
-      use_extra_client = VisionStreamType.VISION_STREAM_WIDE_ROAD in available_streams and VisionStreamType.VISION_STREAM_ROAD in available_streams
-      main_wide_camera = VisionStreamType.VISION_STREAM_ROAD not in available_streams
-      break
-    time.sleep(.1)
-
-  vipc_client_main_stream = VisionStreamType.VISION_STREAM_WIDE_ROAD if main_wide_camera else VisionStreamType.VISION_STREAM_ROAD
-  vipc_client_main = VisionIpcClient("camerad", vipc_client_main_stream, True, cl_context)
-  vipc_client_extra = VisionIpcClient("camerad", VisionStreamType.VISION_STREAM_WIDE_ROAD, False, cl_context)
-  cloudlog.warning(f"vision stream set up, main_wide_camera: {main_wide_camera}, use_extra_client: {use_extra_client}")
-
-  while not vipc_client_main.connect(False):
-    time.sleep(0.1)
-  while use_extra_client and not vipc_client_extra.connect(False):
-    time.sleep(0.1)
-
-  cloudlog.warning(f"connected main cam with buffer size: {vipc_client_main.buffer_len} ({vipc_client_main.width} x {vipc_client_main.height})")
-  if use_extra_client:
-    cloudlog.warning(f"connected extra cam with buffer size: {vipc_client_extra.buffer_len} ({vipc_client_extra.width} x {vipc_client_extra.height})")
-
-  # messaging
-  pm = PubMaster(["modelV2", "cameraOdometry"])
-  sm = SubMaster(["lateralPlan", "roadCameraState", "liveCalibration", "driverMonitoringState", "navModel", "navInstruction"])
-
-  publish_state = PublishState()
-  frame_id = 0
- 
-  buf_main, buf_extra = None, None
-  meta_main = FrameMeta()
-  meta_extra = FrameMeta()
-
   raw_preds = []
   raw_preds_prev = []
   total_cnt = 0
   total_err_cnt = 0
   while True:
-    # Keep receiving frames until we are at least 1 frame ahead of previous extra frame
-    while meta_main.timestamp_sof < meta_extra.timestamp_sof + 25000000:
-      buf_main = vipc_client_main.recv()
-      meta_main = FrameMeta(vipc_client_main)
-      if buf_main is None:
-        break
-
-    if buf_main is None:
-      cloudlog.error("vipc_client_main no frame")
-      continue
-
-    if use_extra_client:
-      # Keep receiving extra frames until frame id matches main camera
-      while True:
-        buf_extra = vipc_client_extra.recv()
-        meta_extra = FrameMeta(vipc_client_extra)
-        if buf_extra is None or meta_main.timestamp_sof < meta_extra.timestamp_sof + 25000000:
-          break
-
-      if buf_extra is None:
-        cloudlog.error("vipc_client_extra no frame")
-        continue
-
-      if abs(meta_main.timestamp_sof - meta_extra.timestamp_sof) > 10000000:
-        cloudlog.error("frames out of sync! main: {} ({:.5f}), extra: {} ({:.5f})".format(
-          meta_main.frame_id, meta_main.timestamp_sof / 1e9,
-          meta_extra.frame_id, meta_extra.timestamp_sof / 1e9))
-
-    else:
-      # Use single camera
-      buf_extra = buf_main
-      meta_extra = meta_main
 
     # TODO: path planner timeout?
-    sm.update(0)
-    desire = sm["lateralPlan"].desire.raw
-    is_rhd = sm["driverMonitoringState"].isRHD
-    frame_id = sm["roadCameraState"].frameId
-
-    traffic_convention = np.zeros(2)
-    traffic_convention[int(is_rhd)] = 1
-
-    vec_desire = np.zeros(ModelConstants.DESIRE_LEN, dtype=np.float32)
-    if desire >= 0 and desire < ModelConstants.DESIRE_LEN:
-      vec_desire[desire] = 1
     
     mt1 = time.perf_counter()
     model_output = model.run()
@@ -223,17 +146,6 @@ def main():
 
     mt2 = time.perf_counter()
     model_execution_time = mt2 - mt1
-
-    if model_output is not None:
-      modelv2_send = messaging.new_message('modelV2')
-      posenet_send = messaging.new_message('cameraOdometry')
-      fill_model_msg(modelv2_send, model_output, publish_state, meta_main.frame_id, meta_extra.frame_id, frame_id, 0.0,
-                      meta_main.timestamp_eof, 0.0, model_execution_time, False, True)
-
-      fill_pose_msg(posenet_send, model_output, meta_main.frame_id, 0.0, meta_main.timestamp_eof, True)
-      pm.send('modelV2', modelv2_send)
-      pm.send('cameraOdometry', posenet_send)
-
 
 if __name__ == "__main__":
   try:
--- selfdrive/test/process_replay/find_modeld_bug3.py
+++ selfdrive/test/process_replay/find_modeld_bug3.py
@@ -57,74 +57,10 @@ def model_replay(lr):
   sm = messaging.SubMaster(['modelV2', 'driverStateV2'])
   pm = messaging.PubMaster(['roadCameraState', 'wideRoadCameraState', 'driverCameraState', 'liveCalibration', 'lateralPlan'])
 
-  managed_processes['modeld'].start()
-  time.sleep(5)
-  sm.update(1000)
-
-  log_msgs = []
-  last_desire = None
-  recv_cnt = defaultdict(int)
-  frame_idxs = defaultdict(int)
-
-  # init modeld with valid calibration
-  cal_msgs = [msg for msg in lr if msg.which() == "liveCalibration"]
-  for _ in range(5):
-    pm.send(cal_msgs[0].which(), cal_msgs[0].as_builder())
-    time.sleep(0.1)
-
-  msgs = defaultdict(list)
-  for msg in lr:
-    msgs[msg.which()].append(msg)
-
-  img = None
-  cam_msgs = list(zip_longest(msgs['roadCameraState'], msgs['wideRoadCameraState']))[0]
-  
-  i = 0
-  img = np.zeros(3493536, dtype=np.uint8)
   while True:
-  #for _ in tqdm(range(600)):
-  #for cam_msgs in zip_longest(msgs['roadCameraState'], msgs['wideRoadCameraState']):
-    # need a pair of road/wide msgs
-    if None in (cam_msgs[0], cam_msgs[1]):
-      break
-
-    for msg in cam_msgs:
-      if msg is None:
-        continue
-
-      if msg.which() in VIPC_STREAM:
-        msg = msg.as_builder()
-        camera_state = getattr(msg, msg.which())
-        
-        #img = frs[msg.which()].get(frame_idxs[msg.which()], pix_fmt="nv12")[0]
-        frame_idxs[msg.which()] += 1
-
-        # send camera state and frame
-        camera_state.frameId = frame_idxs[msg.which()]
-        camera_state.timestampSof = camera_state.timestampSof + int(i * 0.05 * 1e9)
-        camera_state.timestampEof = camera_state.timestampEof + int(i * 0.05 * 1e9)
-        #print(msg)
-        pm.send(msg.which(), msg)
-        vipc_server.send(VIPC_STREAM[msg.which()], img.flatten().tobytes(), camera_state.frameId,
-                          camera_state.timestampSof, camera_state.timestampEof)
-
-        recv = None
-        if min(frame_idxs['roadCameraState'], frame_idxs['wideRoadCameraState']) > recv_cnt['modelV2']:
-          recv = "modelV2"
-        #print(frame_idxs['roadCameraState'], frame_idxs['wideRoadCameraState'], recv_cnt['modelV2'])
-
-        
-        # wait for a response
-        with Timeout(15, f"timed out waiting for {recv}"):
-          if recv:
-            recv_cnt[recv] += 1
-            log_msgs.append(messaging.recv_one(sm.sock[recv]))
-            #print(log_msgs[-1])
-      
-    #if min(frame_idxs['roadCameraState'], frame_idxs['wideRoadCameraState']) > MAX_FRAMES:
-    #  break
-    i += 1
-  managed_processes['modeld'].stop()
+    managed_processes['modeld'].start()
+    time.sleep(10)
+    managed_processes['modeld'].stop()
 
 
   return log_msgs
